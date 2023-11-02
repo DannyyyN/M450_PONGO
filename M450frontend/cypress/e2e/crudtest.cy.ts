6d7d606c00164d464c5f235f66dcb974def504db
DannyyyN <111340578+DannyyyN@users.noreply.github.com>
2023-11-02 14:54:03 +0100

    Update crudtest.cy.ts

--- M450frontend/cypress/e2e/crudtest.cy.ts
+++ M450frontend/cypress/e2e/crudtest.cy.ts
@@ -4,7 +4,7 @@ Cypress.Commands.add('login', () => {
   // Send a login request
   cy.request({
     method: 'POST',
-    url: '${Cypress.env(\'API_URL\')}/user/login', // Adjust the URL to match your login endpoint
+    url: `${Cypress.env('API_URL')}/user/login`, // Adjust the URL to match your login endpoint
     body: {
       email: 'admin@example.com',
       password: '1234',
@@ -12,7 +12,7 @@ Cypress.Commands.add('login', () => {
   }).then((response) => {
     // Extract and save the token and user data from the response
     const { token, user } = response.body;
-    window.localStorage.setItem('token', `Bearer ${token}`);
+    window.localStorage.setItem('token', 'Bearer ${token}');
     window.localStorage.setItem('user', JSON.stringify(user));
   });
 });
@@ -25,10 +25,10 @@ describe('Login', () => {
   it('Visits the login page', () => {
     cy.visit(`${Cypress.env('BASE_URL')}/login/`);
 
-    cy.contains('Sign In`);
+    cy.contains('Sign In');
 
-    cy.get('input[id=email]').type('admin@example.com`);
-    cy.get('input[id=password]').type('1234`);
+    cy.get('input[id=email]').type('admin@example.com');
+    cy.get('input[id=password]').type('1234');
     cy.get('[id=signIn]').click();
     cy.visit(`${Cypress.env('BASE_URL')}/profile`);
     cy.reload();
@@ -39,15 +39,15 @@ describe('CRUD', () => {
   it('Creates an item', () => {
     cy.visit(`${Cypress.env('BASE_URL')}/profile`);
 
-    cy.contains('Welcome`);
+    cy.contains('Welcome');
     cy.contains('Add').click();
 
     cy.visit(`${Cypress.env('BASE_URL')}/itemedit/`);
 
-    cy.get('input[id=name]').type('Aryan Phone`);
-    cy.get('input[id=pictureURL]').type('https://scontent-fra5-2.xx.fbcdn.net/v/t39.30808-6/298803267_752102452891770_203060016891199193_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_ohc=dEgtXTl-ICUAX-fO3iJ&_nc_ht=scontent-fra5-2.xx&oh=00_AfA7STSHqdiqEwkL9PGdfPQHWoGqkGFYcnKpaBGW0XvQ9A&oe=6545A834`);
-    cy.get('input[id=description]').type('Aryan style phone`);
-    cy.get('input[id=price]').type('800`);
+    cy.get('input[id=name]').type('Aryan Phone');
+    cy.get('input[id=pictureURL]').type('https://scontent-fra5-2.xx.fbcdn.net/v/t39.30808-6/298803267_752102452891770_203060016891199193_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_ohc=dEgtXTl-ICUAX-fO3iJ&_nc_ht=scontent-fra5-2.xx&oh=00_AfA7STSHqdiqEwkL9PGdfPQHWoGqkGFYcnKpaBGW0XvQ9A&oe=6545A834');
+    cy.get('input[id=description]').type('Aryan style phone');
+    cy.get('input[id=price]').type('800');
     cy.contains('Finish').click();
     cy.visit(`${Cypress.env('BASE_URL')}/item`);
     cy.reload();
@@ -56,7 +56,7 @@ describe('CRUD', () => {
   it('See all profiles', () => {
     cy.visit(`${Cypress.env('BASE_URL')}/profile`);
 
-    cy.contains('Welcome`);
+    cy.contains('Welcome');
     cy.get('.openDrawer').click();
     cy.contains('Articles').click();
 
@@ -64,15 +64,15 @@ describe('CRUD', () => {
 
     cy.reload();
 
-    cy.contains('Aryan Phone`);
+    cy.contains('Aryan Phone');
     cy.contains('Back').click();
-    cy.visit(`${Cypress.env('BASE_URL')}/profile')
+    cy.visit(`${Cypress.env('BASE_URL')}/profile`);
   });
 
   it('Edit an item', () => {
     cy.visit(`${Cypress.env('BASE_URL')}/profile`);
 
-    cy.contains('Welcome`);
+    cy.contains('Welcome');
     cy.get('.openDrawer').click();
     cy.contains('Articles').click();
 
@@ -82,18 +82,18 @@ describe('CRUD', () => {
 
     cy.contains('Aryan Phone').click();
     cy.contains('Edit').click({multiple: true});
-    cy.get('input[id=name]').clear().type('Arbon Phone`);
-    cy.get('input[id=pictureURL]').clear().type('https://feinekekse.web.app/images/Aryan.jpg`);
-    cy.get('input[id=description]').clear().type('Arbon style phone`);
-    cy.get('input[id=price]').clear().type('60`);
+    cy.get('input[id=name]').clear().type('Arbon Phone');
+    cy.get('input[id=pictureURL]').clear().type('https://feinekekse.web.app/images/Aryan.jpg');
+    cy.get('input[id=description]').clear().type('Arbon style phone');
+    cy.get('input[id=price]').clear().type('60');
     cy.contains('Save').click();
-    cy.visit(`${Cypress.env('BASE_URL')}/item')
+    cy.visit(`${Cypress.env('BASE_URL')}/item`)
   });
 
   it('Delete an item', () => {
     cy.visit(`${Cypress.env('BASE_URL')}/profile`);
 
-    cy.contains('Welcome`);
+    cy.contains('Welcome');
 
     cy.reload();
 
@@ -106,9 +106,9 @@ describe('Logout', () => {
   it('Logout', () => {
     cy.visit(`${Cypress.env('BASE_URL')}/profile`);
 
-    cy.contains('Welcome`);
+    cy.contains('Welcome');
 
     cy.contains('Logout').click();
-    cy.visit(`${Cypress.env('BASE_URL')}/')
+    cy.visit(`${Cypress.env('BASE_URL')}/`)
   })
 });
